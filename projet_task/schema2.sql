afee8153643793bcc03956276c6fef81886d97ad
Evranegct <evrane.guincetre@gmail.com>
2026-01-22 09:31:29 +0100

    Update schema2.sql

--- projet_task/schema2.sql
+++ projet_task/schema2.sql
@@ -1,1 +1,9 @@
+DROP TABLE IF EXISTS tasks;
 
+CREATE TABLE tasks (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    titre TEXT NOT NULL,
+    description TEXT NOT NULL,
+    date_echeance TEXT, -- SQLite stocke les dates sous forme de texte (YYYY-MM-DD)
+    est_terminee INTEGER DEFAULT 0 -- 0 = En cours, 1 = Terminée
+);
